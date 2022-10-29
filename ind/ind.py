35f01a63e522a13f2b09d3eaa1774bcc21916b0e
Artem0622 <kwilmajor707@gmail.com>
2022-10-29 08:56:56 +0300

    save ind

--- ind/ind.py
+++ ind/ind.py
@@ -1,14 +1,20 @@
-if __name__ == '__main__':
-    u = set("abcdefghijklmnopqrstuvwxyz")
-a={"a","b","h","j","l"}
-b={"b","c","h","l","r","u"}
-c={"j","k","n","t","z"}
-d={"b","i","k","u","w"}
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-x=(a.union(b)).intersection(c)
-print(f"x={x}")
-#Дополнение множеств
-an=u.difference(b)
-bn=u.difference(a)
-y=(an.intersection(bn)).difference(c.union(d))
-print(f"y={y}")
+if __name__ == '__main__':
+    # Определим универсальное множество
+    u = set("abcdefghijklmnopqrstuvwxyz")
+    a = {"a", "b", "h", "j", "l"}
+    b = {"b", "c", "h", "l", "r", "u"}
+    c = {"j", "k", "n", "t", "z"}
+    d = {"b", "i", "k", "u", "w"}
+
+    x = (a.union(b)).intersection(c)
+    print(f"x={x}")
+
+    # Дополнение множеств
+    an = u.difference(b)
+    bn = u.difference(a)
+    y = (an.intersection(bn)).difference(c.union(d))
+
+    print(f"y={y}")
